dcce93a9efdeb2bdadbef0c7104d019c2652a508
jingzhou Tao <jingzhoutao@jingzhoudeMacBook-Air.local>
2023-03-21 16:20:21 -0700

    update description

--- src/components/result.js
+++ src/components/result.js
@@ -12,7 +12,7 @@ function Result(){
                 ? "Congratulations! You Correctly Guessed the Wordle"
                 : "You Failed to Guess the Word"}
             </h3>
-            <h3>If you want to take another game,reopen the page</h3>
+            <h3>If you want to take another game,return to Home section and refresh the page</h3>
             <h1>Correct Word: {correctWord}</h1>
             {gameOver.guessedWord && (
             <h3>You guessed in {currAttempt.attempt} attempts</h3>
